61899876c75cf08eadbc0826b385cc4ab5eb7635
gervasek <gervasek@users.noreply.github.com>
2019-07-03 14:02:57 +0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,11 @@ jinja2 /scripts/directors.vcl.jinja2 ${YAML_CONF} > /tmp/directors.vcl
 
 cat  /tmp/directors.vcl
 
-/usr/sbin/varnishd -f /etc/varnish/default.vcl -a 0.0.0.0:${HTTP_PORT} -s malloc,${MAX_MEMORY} -F $@
+# Get our command to run
+export CMD=$@
+
+if [ -z "${CMD}" ]; then
+  /usr/sbin/varnishd -f /etc/varnish/default.vcl -a 0.0.0.0:${HTTP_PORT} -s malloc,${MAX_MEMORY} -F $@
+else
+  exec ${CMD}
+fi

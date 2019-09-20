1d07fcc22933884c035f93794a2943d56599b7f2
Tiago-FM <tiago.mauricio@ext.ec.europa.eu>
2019-09-20 09:28:48 +0200

    Modify run.sh to load yaml only on startup
    Don't load yaml if no command is passed

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-[ ! -f "${YAML_CONF}" ] && echo "Please define backends in a yaml and include YAML_CONF" && exit 1
-
-jinja2 /scripts/directors.vcl.jinja2 ${YAML_CONF} > /tmp/directors.vcl
-
-cat  /tmp/directors.vcl
-
 # Get our command to run
 export CMD=$@
 
 if [ -z "${CMD}" ]; then
+  [ ! -f "${YAML_CONF}" ] && echo "Please define backends in a yaml and include YAML_CONF" && exit 1
+  jinja2 /scripts/directors.vcl.jinja2 ${YAML_CONF} > /tmp/directors.vcl
+  cat  /tmp/directors.vcl
   /usr/sbin/varnishd -f /etc/varnish/default.vcl -a 0.0.0.0:${HTTP_PORT} -s malloc,${MAX_MEMORY} -F $@
 else
   exec ${CMD}
